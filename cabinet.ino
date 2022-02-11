d92dcf460a5e712f52ee4f52414c1bd383112a73
John Wade <johnw8017@gmail.com>
2022-02-10 19:47:32 -0500

    Update cabinet.ino

--- cabinet.ino
+++ cabinet.ino
@@ -27,8 +27,17 @@ void setup(){
 void loop(){
   ldr = analogRead(LDR);
 
+  /*
+  The first 2 numbers are the input low and high. For an analogRead on a 10 bit ADC that is 0 for low and 1023 for high. 
+  The other 2 numbers are for the numbers for the output numbers that correspond for the inputs. So if you want 0 output 
+  for 0 input and 255 output for 1023 input the function would be:
+   outNumber = map(inNumber, 0, 1023, 0, 255);
+
+   You can adjust the numbers to give the output that you want. For instance, what is the analogRead output when the LDR is full dark? What is the analogRead output when the LDR is full light? Use those numbers for the in numbers and 0 and 255 for the out numbers.
+  */
   //ldrValue is based on how much light you have hitting your LDR mine is -5, you may have to adjust for your light source to turn off lights!
-   ldrValue = map(ldr, 0, 1023, 100, -5); //100 is max brightness setting of LEDs 
+  
+  ldrValue = map(ldr, 0, 1023, 100, -5); //100 is max brightness setting of LEDs 
   
   Serial.println("ldr: ");
   Serial.println(ldr);
